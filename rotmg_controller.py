b871e57ac4acb212833dedb4756aec3189fb0791
SuedIsProgramming <thesiud24@gmail.com>
2023-06-28 19:34:03 -0400

    Framework

--- rotmg_controller.py
+++ rotmg_controller.py
@@ -1,1 +1,16 @@
+import time as t
+import pyautogui as pauto
+import pygetwindow as gw
+import cv2
 
+t.sleep(1)
+FACES_NOTEPAD = r"C:\Users\thesi\Documents\GitHub\autoRotmg\images\testingLocateWindow.bmp"
+
+loc = pauto.locateOnWindow(FACES_NOTEPAD,grayscale=True,title=gw.getActiveWindow().title) #Unsure why its not working
+#loc = pauto.locateOnScreen(FACES_NOTEPAD,grayscale=True)
+
+if  loc is None:
+    print('Can\'t find it')
+else:
+    print('Found it at', loc) 
+          
