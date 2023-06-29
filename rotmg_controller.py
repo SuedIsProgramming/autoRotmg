79b379637d72c27b2c35494f63f00e64ee8cae11
SuedIsProgramming <thesiud24@gmail.com>
2023-06-29 01:11:46 -0400

    I believe this works

--- rotmg_controller.py
+++ rotmg_controller.py
@@ -1,18 +1,22 @@
 import time as t
+import keyboard
 import pyautogui as pauto
-import pygetwindow as gw
-import pytesseract as ptsrct
 
-t.sleep(1)
-FACES_NOTEPAD = r"C:\Users\thesi\Documents\GitHub\autoRotmg\images\testingLocateWindow.bmp"
-HP_TESARACT_TESTING=r"C:\Users\sueds\Documents\GitHub\autoRotmg\images\testingTesaractHealth.bmp"
 
-#loc = pauto.locateOnWindow(FACES_NOTEPAD,grayscale=True,title=gw.getActiveWindow().title) #Unsure why its not working
-#loc = pauto.locateOnScreen(FACES_NOTEPAD,grayscale=True)
 
-print(ptsrct.image_to_string(HP_TESARACT_TESTING, lang='eng'))
-#if  loc is None:
-#    print('Can\'t find it')
-#else:
-#    print('Found it at', loc) 
-          
+#pauto.displayMousePosition()
+t.sleep(2)
+FIRST = True
+start_time = 0
+print("working...")
+
+while True:
+    print("working...")
+    if pauto.pixelMatchesColor(1616,472,[120,194,55]):
+        continue
+    elif t.time() > start_time + 10 or FIRST:
+        pauto.press("f")
+        start_time = t.time()
+        FIRST = False
+    if keyboard.is_pressed('f10'):
+        break
