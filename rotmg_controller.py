3c933c12cf394ec87ebb71ea65ec04a1e55170a1
SuedIsProgramming <thesiud24@gmail.com>
2023-06-28 21:14:37 -0400

    testing with tessaract

--- rotmg_controller.py
+++ rotmg_controller.py
@@ -1,16 +1,18 @@
 import time as t
 import pyautogui as pauto
 import pygetwindow as gw
-import cv2
+import pytesseract as ptsrct
 
 t.sleep(1)
 FACES_NOTEPAD = r"C:\Users\thesi\Documents\GitHub\autoRotmg\images\testingLocateWindow.bmp"
+HP_TESARACT_TESTING=r"C:\Users\sueds\Documents\GitHub\autoRotmg\images\testingTesaractHealth.bmp"
 
-loc = pauto.locateOnWindow(FACES_NOTEPAD,grayscale=True,title=gw.getActiveWindow().title) #Unsure why its not working
+#loc = pauto.locateOnWindow(FACES_NOTEPAD,grayscale=True,title=gw.getActiveWindow().title) #Unsure why its not working
 #loc = pauto.locateOnScreen(FACES_NOTEPAD,grayscale=True)
 
-if  loc is None:
-    print('Can\'t find it')
-else:
-    print('Found it at', loc) 
+print(ptsrct.image_to_string(HP_TESARACT_TESTING, lang='eng'))
+#if  loc is None:
+#    print('Can\'t find it')
+#else:
+#    print('Found it at', loc) 
           
