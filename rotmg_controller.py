cb4462cfac7a18ce4d4f0ebe0c09e364b0dad3ad
SuedIsProgramming <thesiud24@gmail.com>
2023-06-30 11:19:58 -0400

    Freshing up

--- rotmg_controller.py
+++ rotmg_controller.py
@@ -4,15 +4,14 @@ import pyautogui as pauto
 
 #Note: Must be started as administrator!
 def activate_auto_nexus():
-
-
     """autoNexus based on detecting whether the pixels to the left of "HP" are Red"""
+
     first = True
     start_time = 0
 
     while True:
         if pauto.pixelMatchesColor(1574,473,[198,50,54]):
-            if t.time() > start_time + 10 or first:
+            if t.time() > start_time + 10 or first: # 10 second cooldown in between nexus key press.
                 pdi.press("f")
                 print("Health low, nexusing...")
                 start_time = t.time()
